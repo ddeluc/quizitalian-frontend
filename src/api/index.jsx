5822ab35636139a1b855a44bd0638426e6e85328
ddeluc <dantedeluca3219@icloud.com>
2023-07-20 12:54:01 -0400

    Updating Backend URL

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const backendurl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000'
+
+const API = axios.create({ baseURL: backendurl });
 const TINT_API = axios.create({ baseURL: 'http://localhost:8080' });
 
 export const getModules = (userId) => API.get(`/api/modules/${userId}`);
